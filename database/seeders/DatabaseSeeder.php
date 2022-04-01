8367792d36e381d207a14fe8aac28eb6ef8baa51
irhamsahbana <irhamsahbana@gmail.com>
2022-04-01 11:57:23 +0800

    dev: seeder for branch and user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CategorySeeder::class);
 
-        // $this->dummy();
+        $this->dummy();
         $this->call(BudgetItemGroupSeeder::class);
         $this->call(BudgetItemSeeder::class);
         $this->call(SubBudgetItemSeeder::class);
@@ -25,16 +25,14 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(DummyBranchSeeder::class);
         $this->call(DummyUserSeeder::class);
-        $this->call(DummyDriverSeeder::class);
-        $this->call(DummyMaterialSeeder::class);
-        $this->call(DummyProjectSeeder::class);
-        $this->call(DummyVehicleSeeder::class);
-        $this->call(DummyVendorSeeder::class);
+        // $this->call(DummyDriverSeeder::class);
+        // $this->call(DummyMaterialSeeder::class);
+        // $this->call(DummyProjectSeeder::class);
+        // $this->call(DummyVehicleSeeder::class);
+        // $this->call(DummyVendorSeeder::class);
 
-        // $this->call(DummyMaterialMutationSeeder::class);
-        // $this->call(DummyDebtMutationSeeder::class);
-        $this->call(DummyFuelSeeder::class);
-        $this->call(DummyOrderSeeder::class);
-        $this->call(DummyVoucherSeeder::class);
+        // $this->call(DummyFuelSeeder::class);
+        // $this->call(DummyOrderSeeder::class);
+        // $this->call(DummyVoucherSeeder::class);
     }
 }
